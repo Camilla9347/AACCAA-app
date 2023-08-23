55c974d179ac44a4bd9f9e8a10e3c4213121d92a
Camilla9347 <camilla.pelagalli@studenti.unitn.it>
2023-08-23 16:17:28 +0200

    security packages

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ require('dotenv').config();
 // apply try-catch automatically to all controllers
 require('express-async-errors')
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 // Setup Basic Express Server
 const express = require('express');
 const app = express();
 
 // rest of the packages
-const morgan = require('morgan')
+//const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const AWS = require('aws-sdk')
 
@@ -44,13 +50,25 @@ const sentenceRouter = require('./routes/sentenceRoutes')
 const NotFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.set('trust proxy',1);
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+    })
+);
+
 // Morgan Package: know which routes you are hitting (development phase)
-app.use(morgan('tiny'));
+//app.use(morgan('tiny'));
 
 // setup express.json() middleware
 // access json data in req.body
 // 1
 app.use(express.json());
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // once first request made and cookie got back
 // then with each request, the browser will send that cookie (automatically to the server)
@@ -62,12 +80,10 @@ app.get('/', (req,res) => {
     res.send('AACCAA')
 })
 
-
 // middleware for authentication routes
 app.use('/api/v1/auth', authRouter)
 // all  sentence routes require user to be authenticated
 app.use('/api/v1/sentences',authenticateUser,sentenceRouter)
-
 
 // 3 no routes found: end up here
 app.use(NotFoundMiddleware);
@@ -76,7 +92,6 @@ app.use(NotFoundMiddleware);
 // invoked from successful request, once existing route is hit
 // when custom errors come into play
 app.use(errorHandlerMiddleware);
-
 
 // setup start port variable (4500) and start function
 const port = process.env.PORT || 4500
